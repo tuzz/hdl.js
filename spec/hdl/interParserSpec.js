f754707fbdf45df1706d59398ad12f413323b98f
Chris Patuzzo <chris@patuzzo.co.uk>
2015-02-21 11:13:08 +0000

    The beginnings of an intermediate parser

---
+++ spec/hdl/interParserSpec.js
@@ -0,0 +1,23 @@
+"use strict";
+
+var described_class = require("../../lib/hdl/interParser");
+
+describe("InterParser", function () {
+  describe("parsing an intermediate derived from a truth table", function () {
+    var graph = described_class.parse({
+      inputs: ["a", "b"],
+      outputs: ["out"],
+      table: [
+        [["a", false], ["b", false], ["out", true]],
+        [["a", false], ["b", true],  ["out", true]],
+        [["a", true],  ["b", false], ["out", true]],
+        [["a", true],  ["b", true],  ["out", false]]
+      ]
+    });
+
+    it("has the correct number of nodes and edges", function () {
+      expect(graph.nodes.length).toEqual(8);
+      expect(graph.edges.length).toEqual(16);
+    });
+  });
+});
