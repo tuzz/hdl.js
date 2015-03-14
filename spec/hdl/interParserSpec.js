275f33b9bcbcb463026d21d06bc78a32dec046c3
Chris Patuzzo <chris@patuzzo.co.uk>
2015-03-14 10:50:50 +0000

    Verify the structure of the graph

--- spec/hdl/interParserSpec.js
+++ spec/hdl/interParserSpec.js
@@ -15,7 +15,73 @@ describe("InterParser", function () {
       ]
     });
 
-    it("has the correct number of nodes and edges", function () {
+    it("builds the correct graph", function () {
+      var instance0 = graph.findBy({ name: "instance-0" });
+      var instance1 = graph.findBy({ name: "instance-1" });
+      var instance2 = graph.findBy({ name: "instance-2" });
+      var instance3 = graph.findBy({ name: "instance-3" });
+      var a         = graph.findBy({ name: "a" });
+      var b         = graph.findBy({ name: "b" });
+      var out       = graph.findBy({ name: "out" });
+      var lookup    = graph.findBy({ name: "lookup" });
+
+      expect(instance0.value.type).toEqual("instance");
+      expect(instance1.value.type).toEqual("instance");
+      expect(instance2.value.type).toEqual("instance");
+      expect(instance3.value.type).toEqual("instance");
+      expect(a.value.type).toEqual("input");
+      expect(b.value.type).toEqual("input");
+      expect(out.value.type).toEqual("output");
+      expect(lookup.value.type).toEqual("chip");
+
+      var edges = instance0.outEdges;
+
+      expect(edges[0].destination).toEqual(lookup);
+      expect(edges[1].destination).toEqual(a);
+      expect(edges[2].destination).toEqual(b);
+      expect(edges[3].destination).toEqual(out);
+
+      expect(edges[0].value).toBeUndefined();
+      expect(edges[1].value).toEqual(false);
+      expect(edges[2].value).toEqual(false);
+      expect(edges[3].value).toEqual(true);
+
+      edges = instance1.outEdges;
+
+      expect(edges[0].destination).toEqual(lookup);
+      expect(edges[1].destination).toEqual(a);
+      expect(edges[2].destination).toEqual(b);
+      expect(edges[3].destination).toEqual(out);
+
+      expect(edges[0].value).toBeUndefined();
+      expect(edges[1].value).toEqual(false);
+      expect(edges[2].value).toEqual(true);
+      expect(edges[3].value).toEqual(true);
+
+      edges = instance2.outEdges;
+
+      expect(edges[0].destination).toEqual(lookup);
+      expect(edges[1].destination).toEqual(a);
+      expect(edges[2].destination).toEqual(b);
+      expect(edges[3].destination).toEqual(out);
+
+      expect(edges[0].value).toBeUndefined();
+      expect(edges[1].value).toEqual(true);
+      expect(edges[2].value).toEqual(false);
+      expect(edges[3].value).toEqual(true);
+
+      edges = instance3.outEdges;
+
+      expect(edges[0].destination).toEqual(lookup);
+      expect(edges[1].destination).toEqual(a);
+      expect(edges[2].destination).toEqual(b);
+      expect(edges[3].destination).toEqual(out);
+
+      expect(edges[0].value).toBeUndefined();
+      expect(edges[1].value).toEqual(true);
+      expect(edges[2].value).toEqual(true);
+      expect(edges[3].value).toEqual(false);
+
       expect(graph.nodes.length).toEqual(8);
       expect(graph.edges.length).toEqual(16);
     });
