847c38039ddd4146d784b17da53f1b6941965255
Chris Patuzzo <chris@patuzzo.co.uk>
2015-03-15 13:12:19 +0000

    Don't treat 'clocked' as anything special yet

--- spec/hdl/inputParser/partSpec.js
+++ spec/hdl/inputParser/partSpec.js
@@ -19,10 +19,6 @@ describe("part", function () {
     expect(subject.parse("abc0_(  in0=1, in1 = F, out_=a_ )")).toEqual([
       "abc0_", [["in0", true], ["in1", false], ["out_", "a_"]]
     ]);
-
-    expect(subject.parse("_clocked(a=b, c=0)")).toEqual([
-      "_clocked", [["a", "b"], ["c", false]]
-    ]);
   });
 
   it("rejects invalid", function () {
@@ -31,6 +27,5 @@ describe("part", function () {
     expect(function () { subject.parse("(a=a)"); }).toThrow();
     expect(function () { subject.parse("not(0=in)"); }).toThrow();
     expect(function () { subject.parse("and(a=_, b=1)"); }).toThrow();
-    expect(function () { subject.parse("_clocked(a=b, c=00)"); }).toThrow();
   });
 });
