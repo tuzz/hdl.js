e7ee52136ffb2295e350452b312f25fd22ae0cff
Chris Patuzzo <chris@patuzzo.co.uk>
2015-07-05 00:00:06 +0100

    Upgrade grunt-jasmine-node

--- spec/hdlSpec.js
+++ spec/hdlSpec.js
@@ -1,5 +1,7 @@
 "use strict";
 
+var HDL = require("../lib/hdl");
+
 describe("HDL", function () {
   it("has some tests", function () {
     expect(HDL.hello()).toEqual("world");
--- spec/specHelper.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-
-global.HDL = require("../lib/hdl");
