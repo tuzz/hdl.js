a149799c6769921552c9a65c5fc93a880c6ef9e8
Chris Patuzzo <chris@patuzzo.co.uk>
2015-08-15 17:27:46 +0100

    Evaluate expressions on concrete chips

--- spec/hdl/evaluatorSpec.js
+++ spec/hdl/evaluatorSpec.js
@@ -2,11 +2,13 @@
 
 var describedClass = require("../../lib/hdl/evaluator");
 var Parser = require("../../lib/hdl/parser");
-var Environment = require("../../lib/hdl/environment");
 
 describe("Evaluator", function () {
   describe("lookup chip", function () {
-    var lookup = { name: "lookup", type: "chip" };
+    var lookup = { value: { name: "lookup", type: "chip" } };
+    var truthy = { name: "truthy", type: "assignment", value: true };
+    var falsey = { name: "falsey", type: "assignment", value: false };
+
     var out = { name: "out", type: "assignment" };
     var x = { name: "x", type: "assignment" };
     var y = { name: "y", type: "assignment" };
@@ -19,7 +21,7 @@ describe("Evaluator", function () {
 
     it("assigns 'true' when true = true", function () {
       var assignments = [
-        { left: true, right: true },
+        { left: true, right: truthy },
         { left: true, right: out }
       ];
       describedClass.evaluate(lookup, assignments);
@@ -28,7 +30,7 @@ describe("Evaluator", function () {
 
     it("assigns 'false' when true = true", function () {
       var assignments = [
-        { left: true, right: true },
+        { left: true, right: truthy },
         { left: false, right: out }
       ];
       describedClass.evaluate(lookup, assignments);
@@ -37,7 +39,7 @@ describe("Evaluator", function () {
 
     it("does not assign when true = false", function () {
       var assignments = [
-        { left: true, right: false },
+        { left: true, right: falsey },
         { left: true, right: out }
       ];
       describedClass.evaluate(lookup, assignments);
@@ -46,7 +48,7 @@ describe("Evaluator", function () {
 
     it("does not assign when false = true", function () {
       var assignments = [
-        { left: false, right: true },
+        { left: false, right: truthy },
         { left: false, right: out }
       ];
       describedClass.evaluate(lookup, assignments);
@@ -55,7 +57,7 @@ describe("Evaluator", function () {
 
     it("assigns 'true' and 'false' when true = true", function () {
       var assignments = [
-        { left: true, right: true },
+        { left: true, right: truthy },
         { left: true, right: x },
         { left: false, right: y }
       ];
@@ -66,14 +68,76 @@ describe("Evaluator", function () {
 
     it("does not assign when when true = true and true = false", function () {
       var assignments = [
-        { left: true, right: true },
-        { left: true, right: false },
+        { left: true, right: truthy },
+        { left: true, right: falsey },
         { left: false, right: x },
         { left: true, right: y }
       ];
       describedClass.evaluate(lookup, assignments);
       expect(x.value).toEqual("initial value");
       expect(y.value).toEqual("initial value");
+    });
+  });
+
+  describe("concrete chips", function () {
+    var nand = Parser.parse("nand", " \n\
+      inputs a, b                     \n\
+      outputs out                     \n\
+                                      \n\
+      | a | b | out |                 \n\
+      | 0 | 0 |  1  |                 \n\
+      | 0 | 1 |  1  |                 \n\
+      | 1 | 0 |  1  |                 \n\
+      | 1 | 1 |  0  |                 \n\
+    ");
+    var chip = nand.findBy({ name: "nand" });
+
+    var a = { name: "a", type: "assignment" };
+    var b = { name: "b", type: "assignment" };
+    var out = { name: "out", type: "assignment" };
+
+    var assignments = [
+      { left: "a", right: a },
+      { left: "b", right: b },
+      { left: "out", right: out }
+    ];
+
+    beforeEach(function () {
+      a.value = "inital value";
+      b.value = "inital value";
+      out.value = "inital value";
+    });
+
+    it("evaluates a = false, b = false correctly", function () {
+      a.value = false;
+      b.value = false;
+
+      describedClass.evaluate(chip, assignments);
+      expect(out.value).toEqual(true);
+    });
+
+    it("evaluates a = false, b = true correctly", function () {
+      a.value = false;
+      b.value = true;
+
+      describedClass.evaluate(chip, assignments);
+      expect(out.value).toEqual(true);
+    });
+
+    it("evaluates a = true, b = false correctly", function () {
+      a.value = true;
+      b.value = false;
+
+      describedClass.evaluate(chip, assignments);
+      expect(out.value).toEqual(true);
+    });
+
+    it("evaluates a = true, b = true correctly", function () {
+      a.value = true;
+      b.value = true;
+
+      describedClass.evaluate(chip, assignments);
+      expect(out.value).toEqual(false);
     });
   });
 });
