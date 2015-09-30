825b8c1b1319ee6f02f0394b8a77bf5964223c77
Chris Patuzzo <chris@patuzzo.co.uk>
2015-09-30 16:31:48 +0100

    Compile CNF expressions for chips made of “parts”

--- spec/hdl/cnfCompilerSpec.js
+++ spec/hdl/cnfCompilerSpec.js
@@ -25,7 +25,7 @@ describe("CNFCompiler", function () {
       inputs a, b                     \n\
       outputs out                     \n\
                                       \n\
-      not(in=x, out=out)              \n\
+      nand(a=x, b=x, out=out)         \n\
       nand(a=a, b=b, out=x)           \n\
     ");
 
@@ -33,7 +33,7 @@ describe("CNFCompiler", function () {
     environment.addChip("and", and);
   });
 
-  it("compiles the CNF expression for the given chip", function () {
+  it("compiles CNF expressions for truth table chips", function () {
     var expression = describedClass.compile("nand", environment);
 
     expect(expression.conjunctions.length).toEqual(4);
@@ -75,6 +75,93 @@ describe("CNFCompiler", function () {
     expect(disjunctions[0].value).toEqual("a");
     expect(disjunctions[0].isNegation).toEqual(true);
     expect(disjunctions[1].value).toEqual("b");
+    expect(disjunctions[1].isNegation).toEqual(true);
+    expect(disjunctions[2].value).toEqual("out");
+    expect(disjunctions[2].isNegation).toEqual(true);
+  });
+
+  it("compiles CNF expressions for chips", function () {
+    var expression = describedClass.compile("and", environment);
+
+    expect(expression.conjunctions.length).toEqual(8);
+    var conjunctions = expression.conjunctions;
+
+    expect(conjunctions[0].disjunctions.length).toEqual(3);
+    var disjunctions = conjunctions[0].disjunctions;
+
+    expect(disjunctions[0].value).toEqual("a");
+    expect(disjunctions[0].isNegation).toEqual(false);
+    expect(disjunctions[1].value).toEqual("b");
+    expect(disjunctions[1].isNegation).toEqual(false);
+    expect(disjunctions[2].value).toEqual("x");
+    expect(disjunctions[2].isNegation).toEqual(false);
+
+    expect(conjunctions[1].disjunctions.length).toEqual(3);
+    disjunctions = conjunctions[1].disjunctions;
+
+    expect(disjunctions[0].value).toEqual("a");
+    expect(disjunctions[0].isNegation).toEqual(false);
+    expect(disjunctions[1].value).toEqual("b");
+    expect(disjunctions[1].isNegation).toEqual(true);
+    expect(disjunctions[2].value).toEqual("x");
+    expect(disjunctions[2].isNegation).toEqual(false);
+
+    expect(conjunctions[2].disjunctions.length).toEqual(3);
+    disjunctions = conjunctions[2].disjunctions;
+
+    expect(disjunctions[0].value).toEqual("a");
+    expect(disjunctions[0].isNegation).toEqual(true);
+    expect(disjunctions[1].value).toEqual("b");
+    expect(disjunctions[1].isNegation).toEqual(false);
+    expect(disjunctions[2].value).toEqual("x");
+    expect(disjunctions[2].isNegation).toEqual(false);
+
+    expect(conjunctions[3].disjunctions.length).toEqual(3);
+    disjunctions = conjunctions[3].disjunctions;
+
+    expect(disjunctions[0].value).toEqual("a");
+    expect(disjunctions[0].isNegation).toEqual(true);
+    expect(disjunctions[1].value).toEqual("b");
+    expect(disjunctions[1].isNegation).toEqual(true);
+    expect(disjunctions[2].value).toEqual("x");
+    expect(disjunctions[2].isNegation).toEqual(true);
+
+    expect(conjunctions[4].disjunctions.length).toEqual(3);
+    disjunctions = conjunctions[4].disjunctions;
+
+    expect(disjunctions[0].value).toEqual("x");
+    expect(disjunctions[0].isNegation).toEqual(false);
+    expect(disjunctions[1].value).toEqual("x");
+    expect(disjunctions[1].isNegation).toEqual(false);
+    expect(disjunctions[2].value).toEqual("out");
+    expect(disjunctions[2].isNegation).toEqual(false);
+
+    expect(conjunctions[5].disjunctions.length).toEqual(3);
+    disjunctions = conjunctions[5].disjunctions;
+
+    expect(disjunctions[0].value).toEqual("x");
+    expect(disjunctions[0].isNegation).toEqual(false);
+    expect(disjunctions[1].value).toEqual("x");
+    expect(disjunctions[1].isNegation).toEqual(true);
+    expect(disjunctions[2].value).toEqual("out");
+    expect(disjunctions[2].isNegation).toEqual(false);
+
+    expect(conjunctions[6].disjunctions.length).toEqual(3);
+    disjunctions = conjunctions[6].disjunctions;
+
+    expect(disjunctions[0].value).toEqual("x");
+    expect(disjunctions[0].isNegation).toEqual(true);
+    expect(disjunctions[1].value).toEqual("x");
+    expect(disjunctions[1].isNegation).toEqual(false);
+    expect(disjunctions[2].value).toEqual("out");
+    expect(disjunctions[2].isNegation).toEqual(false);
+
+    expect(conjunctions[7].disjunctions.length).toEqual(3);
+    disjunctions = conjunctions[7].disjunctions;
+
+    expect(disjunctions[0].value).toEqual("x");
+    expect(disjunctions[0].isNegation).toEqual(true);
+    expect(disjunctions[1].value).toEqual("x");
     expect(disjunctions[1].isNegation).toEqual(true);
     expect(disjunctions[2].value).toEqual("out");
     expect(disjunctions[2].isNegation).toEqual(true);
