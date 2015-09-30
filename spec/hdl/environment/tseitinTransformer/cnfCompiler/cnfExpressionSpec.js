076eb01fc5e1737068d9bc4a29fcc78e3c592cb1
Chris Patuzzo <chris@patuzzo.co.uk>
2015-09-30 19:26:40 +0100

    Add a #toString method for cnfExpressions

--- spec/hdl/environment/tseitinTransformer/cnfCompiler/cnfExpressionSpec.js
+++ spec/hdl/environment/tseitinTransformer/cnfCompiler/cnfExpressionSpec.js
@@ -1,7 +1,7 @@
 "use strict";
 
-var env = "../../../../../lib/hdl/environment";
-var CNFExpression = require(env + "/tseitinTransformer/cnfExpression");
+var tseitin = "../../../../../lib/hdl/environment/tseitinTransformer";
+var CNFExpression = require(tseitin + "/cnfExpression");
 
 describe("CNFExpression", function () {
   it("represents a CNF expression", function () {
@@ -10,8 +10,35 @@ describe("CNFExpression", function () {
     var disjunction = new CNFExpression.Disjunction();
 
     disjunction.value = "a";
-    disjunction.isNegative = false;
+    disjunction.isNegation = false;
     conjunction.disjunctions.push(disjunction);
     expression.conjunctions.push(conjunction);
+  });
+
+  it("prints out to a human-readable form", function () {
+    var expression = new CNFExpression();
+    var conjunction = new CNFExpression.Conjunction();
+    var disjunction = new CNFExpression.Disjunction();
+
+    disjunction.value = "a";
+    disjunction.isNegation = false;
+    conjunction.disjunctions.push(disjunction);
+
+    disjunction = new CNFExpression.Disjunction();
+    disjunction.value = "b";
+    disjunction.isNegation = true;
+    conjunction.disjunctions.push(disjunction);
+
+    expression.conjunctions.push(conjunction);
+    conjunction = new CNFExpression.Conjunction();
+
+    disjunction = new CNFExpression.Disjunction();
+    disjunction.value = "x";
+    disjunction.isNegation = false;
+    conjunction.disjunctions.push(disjunction);
+
+    expression.conjunctions.push(conjunction);
+
+    expect(expression.toString()).toEqual("(a || !b) && x");
   });
 });
