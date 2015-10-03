b09467acb26c83fd5f1b6dd147fac6cfb997b245
Chris Patuzzo <chris@patuzzo.co.uk>
2015-10-03 14:09:45 +0100

    Define chips for letter lookups...
    The 'i' pins combined denote a binary number
    between 0 and 63.
    
    The 'o' pins combined denote a binary number
    between 0 and 7.
    
    Let's take an example from the 'e_lookup' chip:
    
    | 0 | 0 | 0 | 1 | 0 | 1 | 0 | 1 | 0 |
    
    This row maps the input number 5 to the output
    number 2.
    
    This is because the phrase "five e's" contains two
    'e' characters.
    
    Note that the 's_lookup' chip is special, because
    of the trailing 's'. It adds an extra 's' to every
    number except one, which would not result in a
    trailing 's'.

---
+++ spec/chips/aLookupSpec.js
@@ -0,0 +1,30 @@
+"use strict";
+
+var describedChip = "a_lookup";
+var HDL = require("../../lib/hdl");
+var fs = require("fs");
+
+describe(describedChip, function () {
+  beforeEach(function () {
+    var root = [__dirname, "..", ".."].join("/");
+    var path = [root, "chips", describedChip + ".hdl"].join("/");
+    var hdl = fs.readFileSync(path).toString();
+
+    HDL.reset();
+    HDL.define(describedChip, hdl);
+  });
+
+  it("behaves as expected", function () {
+    var result = HDL.evaluate(describedChip, {
+      i5: false, // 32 * 0
+      i4: false, // 16 * 0
+      i3: false, // 08 * 0
+      i2: true,  // 04 * 1
+      i1: false, // 02 * 0
+      i0: true   // 01 * 1
+    });
+
+    // The term "five a's" contains 1 'a'
+    expect(result).toEqual({ o2: false, o1: false, o0: true });
+  });
+});
---
+++ spec/chips/eLookupSpec.js
@@ -0,0 +1,51 @@
+"use strict";
+
+var describedChip = "e_lookup";
+var HDL = require("../../lib/hdl");
+var fs = require("fs");
+
+describe(describedChip, function () {
+  beforeEach(function () {
+    var root = [__dirname, "..", ".."].join("/");
+    var path = [root, "chips", describedChip + ".hdl"].join("/");
+    var hdl = fs.readFileSync(path).toString();
+
+    HDL.reset();
+    HDL.define(describedChip, hdl);
+  });
+
+  it("behaves as expected", function () {
+    var result = HDL.evaluate(describedChip, {
+      i5: false, // 32 * 0
+      i4: false, // 16 * 0
+      i3: false, // 08 * 0
+      i2: true,  // 04 * 1
+      i1: false, // 02 * 0
+      i0: true   // 01 * 1
+    });
+    // The term "five e's" contains 2 'e'
+    expect(result).toEqual({ o2: false, o1: true, o0: false });
+
+    result = HDL.evaluate(describedChip, {
+      i5: false, // 32 * 0
+      i4: false, // 16 * 0
+      i3: false, // 08 * 0
+      i2: false, // 04 * 0
+      i1: false, // 02 * 0
+      i0: true   // 01 * 1
+    });
+    // The term "one e" contains 2 'e'
+    expect(result).toEqual({ o2: false, o1: true, o0: false });
+
+    result = HDL.evaluate(describedChip, {
+      i5: false, // 32 * 0
+      i4: true,  // 16 * 1
+      i3: false, // 08 * 0
+      i2: false, // 04 * 0
+      i1: false, // 02 * 0
+      i0: true   // 01 * 1
+    });
+    // The term "seventeen e's" contains 5 'e'
+    expect(result).toEqual({ o2: true, o1: false, o0: true });
+  });
+});
