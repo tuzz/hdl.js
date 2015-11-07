2ada427cf65daee48249130db9ea2fdd99ff3cec
Chris Patuzzo <chris@patuzzo.co.uk>
2015-11-07 11:58:36 +0000

    Reduce the precision of round_decimal

--- spec/chips/roundDecimalSpec.js
+++ spec/chips/roundDecimalSpec.js
@@ -32,174 +32,154 @@ describe(describedChip, function () {
     var _ = false;
     var T = true;
 
-    // 0.000 -> 0.00
+    // 0.00 -> 0.0
     var result = HDL.evaluate(describedChip, {
       sf1_in5: _, sf1_in4: _, sf1_in3: _,
       sf1_in2: _, sf1_in1: _, sf1_in0: _,
 
       sf2_in3: _, sf2_in2: _, sf2_in1: _, sf2_in0: _,
-      sf3_in3: _, sf3_in2: _, sf3_in1: _, sf3_in0: _,
-      sf4_in3: _, sf4_in2: _, sf4_in1: _, sf4_in0: _,
+      sf3_in3: _, sf3_in2: _, sf3_in1: _, sf3_in0: _
     });
     expect(result).toEqual({
       sf1_out5: _, sf1_out4: _, sf1_out3: _,
       sf1_out2: _, sf1_out1: _, sf1_out0: _,
 
-      sf2_out3: _, sf2_out2: _, sf2_out1: _, sf2_out0: _,
-      sf3_out3: _, sf3_out2: _, sf3_out1: _, sf3_out0: _
+      sf2_out3: _, sf2_out2: _, sf2_out1: _, sf2_out0: _
     });
 
-    // 0.004 -> 0.00
+    // 0.04 -> 0.0
     result = HDL.evaluate(describedChip, {
       sf1_in5: _, sf1_in4: _, sf1_in3: _,
       sf1_in2: _, sf1_in1: _, sf1_in0: _,
 
       sf2_in3: _, sf2_in2: _, sf2_in1: _, sf2_in0: _,
-      sf3_in3: _, sf3_in2: _, sf3_in1: _, sf3_in0: _,
-      sf4_in3: _, sf4_in2: T, sf4_in1: _, sf4_in0: _,
+      sf3_in3: _, sf3_in2: T, sf3_in1: _, sf3_in0: _
     });
     expect(result).toEqual({
       sf1_out5: _, sf1_out4: _, sf1_out3: _,
       sf1_out2: _, sf1_out1: _, sf1_out0: _,
 
-      sf2_out3: _, sf2_out2: _, sf2_out1: _, sf2_out0: _,
-      sf3_out3: _, sf3_out2: _, sf3_out1: _, sf3_out0: _
+      sf2_out3: _, sf2_out2: _, sf2_out1: _, sf2_out0: _
     });
 
-    // 0.005 -> 0.01
+    // 0.05 -> 0.1
     result = HDL.evaluate(describedChip, {
       sf1_in5: _, sf1_in4: _, sf1_in3: _,
       sf1_in2: _, sf1_in1: _, sf1_in0: _,
-
-      sf2_in3: _, sf2_in2: _, sf2_in1: _, sf2_in0: _,
-      sf3_in3: _, sf3_in2: _, sf3_in1: _, sf3_in0: _,
-      sf4_in3: _, sf4_in2: T, sf4_in1: _, sf4_in0: T,
-    });
-    expect(result).toEqual({
-      sf1_out5: _, sf1_out4: _, sf1_out3: _,
-      sf1_out2: _, sf1_out1: _, sf1_out0: _,
-
-      sf2_out3: _, sf2_out2: _, sf2_out1: _, sf2_out0: _,
-      sf3_out3: _, sf3_out2: _, sf3_out1: _, sf3_out0: T
-    });
-
-    // 0.009 -> 0.01
-    result = HDL.evaluate(describedChip, {
-      sf1_in5: _, sf1_in4: _, sf1_in3: _,
-      sf1_in2: _, sf1_in1: _, sf1_in0: _,
-
-      sf2_in3: _, sf2_in2: _, sf2_in1: _, sf2_in0: _,
-      sf3_in3: _, sf3_in2: _, sf3_in1: _, sf3_in0: _,
-      sf4_in3: T, sf4_in2: _, sf4_in1: _, sf4_in0: T,
-    });
-    expect(result).toEqual({
-      sf1_out5: _, sf1_out4: _, sf1_out3: _,
-      sf1_out2: _, sf1_out1: _, sf1_out0: _,
-
-      sf2_out3: _, sf2_out2: _, sf2_out1: _, sf2_out0: _,
-      sf3_out3: _, sf3_out2: _, sf3_out1: _, sf3_out0: T
-    });
-
-    // 1.059 -> 1.06
-    result = HDL.evaluate(describedChip, {
-      sf1_in5: _, sf1_in4: _, sf1_in3: _,
-      sf1_in2: _, sf1_in1: _, sf1_in0: T,
 
       sf2_in3: _, sf2_in2: _, sf2_in1: _, sf2_in0: _,
       sf3_in3: _, sf3_in2: T, sf3_in1: _, sf3_in0: T,
-      sf4_in3: T, sf4_in2: _, sf4_in1: _, sf4_in0: T,
     });
     expect(result).toEqual({
       sf1_out5: _, sf1_out4: _, sf1_out3: _,
-      sf1_out2: _, sf1_out1: _, sf1_out0: T,
+      sf1_out2: _, sf1_out1: _, sf1_out0: _,
 
-      sf2_out3: _, sf2_out2: _, sf2_out1: _, sf2_out0: _,
-      sf3_out3: _, sf3_out2: T, sf3_out1: T, sf3_out0: _
+      sf2_out3: _, sf2_out2: _, sf2_out1: _, sf2_out0: T
     });
 
-    // 1.099 -> 1.10
+    // 0.09 -> 0.1
+    result = HDL.evaluate(describedChip, {
+      sf1_in5: _, sf1_in4: _, sf1_in3: _,
+      sf1_in2: _, sf1_in1: _, sf1_in0: _,
+
+      sf2_in3: _, sf2_in2: _, sf2_in1: _, sf2_in0: _,
+      sf3_in3: T, sf3_in2: _, sf3_in1: _, sf3_in0: T
+    });
+    expect(result).toEqual({
+      sf1_out5: _, sf1_out4: _, sf1_out3: _,
+      sf1_out2: _, sf1_out1: _, sf1_out0: _,
+
+      sf2_out3: _, sf2_out2: _, sf2_out1: _, sf2_out0: T,
+    });
+
+    // 1.59 -> 1.6
     result = HDL.evaluate(describedChip, {
       sf1_in5: _, sf1_in4: _, sf1_in3: _,
       sf1_in2: _, sf1_in1: _, sf1_in0: T,
 
-      sf2_in3: _, sf2_in2: _, sf2_in1: _, sf2_in0: _,
-      sf3_in3: T, sf3_in2: _, sf3_in1: _, sf3_in0: T,
-      sf4_in3: T, sf4_in2: _, sf4_in1: _, sf4_in0: T,
+      sf2_in3: _, sf2_in2: T, sf2_in1: _, sf2_in0: T,
+      sf3_in3: T, sf3_in2: _, sf3_in1: _, sf3_in0: T
     });
     expect(result).toEqual({
       sf1_out5: _, sf1_out4: _, sf1_out3: _,
       sf1_out2: _, sf1_out1: _, sf1_out0: T,
 
-      sf2_out3: _, sf2_out2: _, sf2_out1: _, sf2_out0: T,
-      sf3_out3: _, sf3_out2: _, sf3_out1: _, sf3_out0: _
+      sf2_out3: _, sf2_out2: T, sf2_out1: T, sf2_out0: _,
     });
 
-    // 2.899 -> 2.900
+    // 1.99 -> 2.0
+    result = HDL.evaluate(describedChip, {
+      sf1_in5: _, sf1_in4: _, sf1_in3: _,
+      sf1_in2: _, sf1_in1: _, sf1_in0: T,
+
+      sf2_in3: T, sf2_in2: _, sf2_in1: _, sf2_in0: T,
+      sf3_in3: T, sf3_in2: _, sf3_in1: _, sf3_in0: T
+    });
+    expect(result).toEqual({
+      sf1_out5: _, sf1_out4: _, sf1_out3: _,
+      sf1_out2: _, sf1_out1: T, sf1_out0: _,
+
+      sf2_out3: _, sf2_out2: _, sf2_out1: _, sf2_out0: _
+    });
+
+    // 2.89 -> 2.9
     result = HDL.evaluate(describedChip, {
       sf1_in5: _, sf1_in4: _, sf1_in3: _,
       sf1_in2: _, sf1_in1: T, sf1_in0: _,
 
       sf2_in3: T, sf2_in2: _, sf2_in1: _, sf2_in0: _,
-      sf3_in3: T, sf3_in2: _, sf3_in1: _, sf3_in0: T,
-      sf4_in3: T, sf4_in2: _, sf4_in1: _, sf4_in0: T,
+      sf3_in3: T, sf3_in2: _, sf3_in1: _, sf3_in0: T
     });
     expect(result).toEqual({
       sf1_out5: _, sf1_out4: _, sf1_out3: _,
       sf1_out2: _, sf1_out1: T, sf1_out0: _,
 
       sf2_out3: T, sf2_out2: _, sf2_out1: _, sf2_out0: T,
-      sf3_out3: _, sf3_out2: _, sf3_out1: _, sf3_out0: _
     });
 
-    // 2.999 -> 3.000
+    // 2.99 -> 3.0
     result = HDL.evaluate(describedChip, {
       sf1_in5: _, sf1_in4: _, sf1_in3: _,
       sf1_in2: _, sf1_in1: T, sf1_in0: _,
 
       sf2_in3: T, sf2_in2: _, sf2_in1: _, sf2_in0: T,
       sf3_in3: T, sf3_in2: _, sf3_in1: _, sf3_in0: T,
-      sf4_in3: T, sf4_in2: _, sf4_in1: _, sf4_in0: T,
     });
     expect(result).toEqual({
       sf1_out5: _, sf1_out4: _, sf1_out3: _,
       sf1_out2: _, sf1_out1: T, sf1_out0: T,
 
       sf2_out3: _, sf2_out2: _, sf2_out1: _, sf2_out0: _,
-      sf3_out3: _, sf3_out2: _, sf3_out1: _, sf3_out0: _
     });
 
-    // 63.994 -> 63.99
+    // 63.94 -> 63.9
     result = HDL.evaluate(describedChip, {
       sf1_in5: T, sf1_in4: T, sf1_in3: T,
       sf1_in2: T, sf1_in1: T, sf1_in0: T,
 
       sf2_in3: T, sf2_in2: _, sf2_in1: _, sf2_in0: T,
-      sf3_in3: T, sf3_in2: _, sf3_in1: _, sf3_in0: T,
-      sf4_in3: _, sf4_in2: T, sf4_in1: _, sf4_in0: _,
+      sf3_in3: _, sf3_in2: T, sf3_in1: _, sf3_in0: _
     });
     expect(result).toEqual({
       sf1_out5: T, sf1_out4: T, sf1_out3: T,
       sf1_out2: T, sf1_out1: T, sf1_out0: T,
 
-      sf2_out3: T, sf2_out2: _, sf2_out1: _, sf2_out0: T,
-      sf3_out3: T, sf3_out2: _, sf3_out1: _, sf3_out0: T
+      sf2_out3: T, sf2_out2: _, sf2_out1: _, sf2_out0: T
     });
 
-    // 63.995 -> 0.000 (overflows)
+    // 63.95 -> 0.0 (overflows)
     result = HDL.evaluate(describedChip, {
       sf1_in5: T, sf1_in4: T, sf1_in3: T,
       sf1_in2: T, sf1_in1: T, sf1_in0: T,
 
       sf2_in3: T, sf2_in2: _, sf2_in1: _, sf2_in0: T,
-      sf3_in3: T, sf3_in2: _, sf3_in1: _, sf3_in0: T,
-      sf4_in3: _, sf4_in2: T, sf4_in1: _, sf4_in0: T,
+      sf3_in3: _, sf3_in2: T, sf3_in1: _, sf3_in0: T,
     });
     expect(result).toEqual({
       sf1_out5: _, sf1_out4: _, sf1_out3: _,
       sf1_out2: _, sf1_out1: _, sf1_out0: _,
 
       sf2_out3: _, sf2_out2: _, sf2_out1: _, sf2_out0: _,
-      sf3_out3: _, sf3_out2: _, sf3_out1: _, sf3_out0: _
     });
   });
 });
