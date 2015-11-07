895aeff9849b68d9a3371e1520e32f3ecd2e1601
Chris Patuzzo <chris@patuzzo.co.uk>
2015-11-07 12:50:54 +0000

    Reduce the precision of *_number

--- spec/chips/eNumberSpec.js
+++ spec/chips/eNumberSpec.js
@@ -37,139 +37,113 @@ describe(describedChip, function () {
       a_sf1_5: _, a_sf1_4: _, a_sf1_3: _, a_sf1_2: _, a_sf1_1: _, a_sf1_0: T, // one           = 1
                                                                               // point         = 0
       a_sf2_3: T, a_sf2_2: _, a_sf2_1: _, a_sf2_0: T,                         // nine          = 1
-      a_sf3_3: T, a_sf3_2: _, a_sf3_1: _, a_sf3_0: _,                         // eight         = 1
 
       b_sf1_5: _, b_sf1_4: _, b_sf1_3: _, b_sf1_2: _, b_sf1_1: T, b_sf1_0: _, // two           = 0
                                                                               // point         = 0
       b_sf2_3: _, b_sf2_2: T, b_sf2_1: T, b_sf2_0: T,                         // seven         = 2
-      b_sf3_3: _, b_sf3_2: T, b_sf3_1: T, b_sf3_0: _,                         // six           = 0
 
       c_sf1_5: _, c_sf1_4: _, c_sf1_3: _, c_sf1_2: _, c_sf1_1: T, c_sf1_0: T, // three         = 2
                                                                               // point         = 0
       c_sf2_3: _, c_sf2_2: T, c_sf2_1: _, c_sf2_0: T,                         // five          = 1
-      c_sf3_3: _, c_sf3_2: T, c_sf3_1: _, c_sf3_0: _,                         // four          = 0
 
       d_sf1_5: _, d_sf1_4: _, d_sf1_3: _, d_sf1_2: T, d_sf1_1: _, d_sf1_0: _, // four          = 0
                                                                               // point         = 0
       d_sf2_3: _, d_sf2_2: _, d_sf2_1: T, d_sf2_0: T,                         // three         = 2
-      d_sf3_3: _, d_sf3_2: _, d_sf3_1: T, d_sf3_0: _,                         // two           = 0
 
       e_sf1_5: _, e_sf1_4: _, e_sf1_3: _, e_sf1_2: T, e_sf1_1: _, e_sf1_0: T, // five          = 1
                                                                               // point         = 0
       e_sf2_3: _, e_sf2_2: _, e_sf2_1: _, e_sf2_0: T,                         // one           = 1
-      e_sf3_3: _, e_sf3_2: _, e_sf3_1: _, e_sf3_0: _,                         // ____          = 0
 
       f_sf1_5: _, f_sf1_4: _, f_sf1_3: _, f_sf1_2: T, f_sf1_1: T, f_sf1_0: _, // six           = 0
                                                                               // point         = 0
       f_sf2_3: T, f_sf2_2: _, f_sf2_1: _, f_sf2_0: T,                         // nine          = 1
-      f_sf3_3: T, f_sf3_2: _, f_sf3_1: _, f_sf3_0: _,                         // eight         = 1
 
       g_sf1_5: _, g_sf1_4: _, g_sf1_3: _, g_sf1_2: T, g_sf1_1: T, g_sf1_0: T, // seven         = 2
                                                                               // point         = 0
       g_sf2_3: _, g_sf2_2: T, g_sf2_1: T, g_sf2_0: T,                         // seven         = 2
-      g_sf3_3: _, g_sf3_2: T, g_sf3_1: T, g_sf3_0: _,                         // six           = 0
 
       h_sf1_5: _, h_sf1_4: _, h_sf1_3: T, h_sf1_2: _, h_sf1_1: _, h_sf1_0: _, // eight         = 1
                                                                               // point         = 0
       h_sf2_3: _, h_sf2_2: T, h_sf2_1: _, h_sf2_0: T,                         // five          = 1
-      h_sf3_3: _, h_sf3_2: T, h_sf3_1: _, h_sf3_0: _,                         // four          = 0
 
       i_sf1_5: _, i_sf1_4: _, i_sf1_3: T, i_sf1_2: _, i_sf1_1: _, i_sf1_0: T, // nine          = 1
                                                                               // point         = 0
       i_sf2_3: _, i_sf2_2: _, i_sf2_1: T, i_sf2_0: T,                         // three         = 2
-      i_sf3_3: _, i_sf3_2: _, i_sf3_1: T, i_sf3_0: _,                         // two           = 0
 
       j_sf1_5: _, j_sf1_4: _, j_sf1_3: T, j_sf1_2: _, j_sf1_1: T, j_sf1_0: _, // ten           = 1
                                                                               // point         = 0
       j_sf2_3: _, j_sf2_2: _, j_sf2_1: _, j_sf2_0: T,                         // one           = 1
-      j_sf3_3: _, j_sf3_2: _, j_sf3_1: _, j_sf3_0: _,                         // ____          = 0
 
       k_sf1_5: _, k_sf1_4: _, k_sf1_3: T, k_sf1_2: _, k_sf1_1: T, k_sf1_0: T, // eleven        = 3
                                                                               // point         = 0
       k_sf2_3: T, k_sf2_2: _, k_sf2_1: _, k_sf2_0: T,                         // nine          = 1
-      k_sf3_3: T, k_sf3_2: _, k_sf3_1: _, k_sf3_0: _,                         // eight         = 1
 
       l_sf1_5: _, l_sf1_4: _, l_sf1_3: T, l_sf1_2: T, l_sf1_1: _, l_sf1_0: _, // twelve        = 2
                                                                               // point         = 0
       l_sf2_3: _, l_sf2_2: T, l_sf2_1: T, l_sf2_0: T,                         // seven         = 2
-      l_sf3_3: _, l_sf3_2: T, l_sf3_1: T, l_sf3_0: _,                         // six           = 0
 
       m_sf1_5: _, m_sf1_4: _, m_sf1_3: T, m_sf1_2: T, m_sf1_1: _, m_sf1_0: T, // thirteen      = 2
                                                                               // ____          = 0
       m_sf2_3: _, m_sf2_2: _, m_sf2_1: _, m_sf2_0: _,                         // ____          = 0
-      m_sf3_3: _, m_sf3_2: _, m_sf3_1: _, m_sf3_0: _,                         // ____          = 0
 
       n_sf1_5: _, n_sf1_4: _, n_sf1_3: T, n_sf1_2: T, n_sf1_1: T, n_sf1_0: _, // fourteen      = 2
                                                                               // point         = 0
       n_sf2_3: _, n_sf2_2: _, n_sf2_1: T, n_sf2_0: T,                         // three         = 2
-      n_sf3_3: _, n_sf3_2: _, n_sf3_1: T, n_sf3_0: _,                         // two           = 0
 
       o_sf1_5: _, o_sf1_4: _, o_sf1_3: T, o_sf1_2: T, o_sf1_1: T, o_sf1_0: T, // fifteen       = 2
                                                                               // point         = 0
       o_sf2_3: _, o_sf2_2: _, o_sf2_1: _, o_sf2_0: T,                         // one           = 1
-      o_sf3_3: _, o_sf3_2: _, o_sf3_1: _, o_sf3_0: _,                         // ____          = 0
 
       p_sf1_5: _, p_sf1_4: T, p_sf1_3: _, p_sf1_2: _, p_sf1_1: _, p_sf1_0: _, // sixteen       = 2
-                                                                              // point         = 0
-      p_sf2_3: _, p_sf2_2: _, p_sf2_1: _, p_sf2_0: _,                         // zero          = 1
-      p_sf3_3: _, p_sf3_2: T, p_sf3_1: _, p_sf3_0: T,                         // five          = 1
+                                                                              // ----          = 0
+      p_sf2_3: _, p_sf2_2: _, p_sf2_1: _, p_sf2_0: _,                         // ----          = 0
 
       q_sf1_5: _, q_sf1_4: T, q_sf1_3: _, q_sf1_2: _, q_sf1_1: _, q_sf1_0: T, // seventeen     = 4
                                                                               // point         = 0
       q_sf2_3: _, q_sf2_2: T, q_sf2_1: T, q_sf2_0: T,                         // seven         = 2
-      q_sf3_3: _, q_sf3_2: T, q_sf3_1: T, q_sf3_0: _,                         // six           = 0
 
       r_sf1_5: _, r_sf1_4: T, r_sf1_3: _, r_sf1_2: _, r_sf1_1: T, r_sf1_0: _, // eighteen      = 3
                                                                               // point         = 0
       r_sf2_3: _, r_sf2_2: T, r_sf2_1: _, r_sf2_0: T,                         // five          = 1
-      r_sf3_3: _, r_sf3_2: T, r_sf3_1: _, r_sf3_0: _,                         // four          = 0
 
       s_sf1_5: _, s_sf1_4: T, s_sf1_3: _, s_sf1_2: _, s_sf1_1: T, s_sf1_0: T, // nineteen      = 3
                                                                               // point         = 0
       s_sf2_3: _, s_sf2_2: _, s_sf2_1: T, s_sf2_0: T,                         // three         = 2
-      s_sf3_3: _, s_sf3_2: _, s_sf3_1: T, s_sf3_0: _,                         // two           = 0
 
       t_sf1_5: _, t_sf1_4: T, t_sf1_3: _, t_sf1_2: T, t_sf1_1: _, t_sf1_0: _, // twenty        = 1
                                                                               // point         = 0
       t_sf2_3: _, t_sf2_2: _, t_sf2_1: _, t_sf2_0: T,                         // one           = 1
-      t_sf3_3: _, t_sf3_2: _, t_sf3_1: _, t_sf3_0: _,                         // ____          = 0
 
       u_sf1_5: _, u_sf1_4: T, u_sf1_3: _, u_sf1_2: T, u_sf1_1: _, u_sf1_0: T, // twenty-one    = 2
                                                                               // point         = 0
       u_sf2_3: T, u_sf2_2: _, u_sf2_1: _, u_sf2_0: T,                         // nine          = 1
-      u_sf3_3: T, u_sf3_2: _, u_sf3_1: _, u_sf3_0: _,                         // eight         = 1
 
       v_sf1_5: _, v_sf1_4: T, v_sf1_3: _, v_sf1_2: T, v_sf1_1: T, v_sf1_0: _, // twenty-two    = 1
                                                                               // point         = 0
       v_sf2_3: _, v_sf2_2: T, v_sf2_1: T, v_sf2_0: T,                         // seven         = 2
-      v_sf3_3: _, v_sf3_2: T, v_sf3_1: T, v_sf3_0: _,                         // six           = 0
 
       w_sf1_5: _, w_sf1_4: T, w_sf1_3: _, w_sf1_2: T, w_sf1_1: T, w_sf1_0: T, // twenty-three  = 3
                                                                               // ____          = 0
       w_sf2_3: _, w_sf2_2: _, w_sf2_1: _, w_sf2_0: _,                         // ____          = 0
-      w_sf3_3: _, w_sf3_2: _, w_sf3_1: _, w_sf3_0: _,                         // ____          = 0
 
       x_sf1_5: _, x_sf1_4: T, x_sf1_3: T, x_sf1_2: _, x_sf1_1: _, x_sf1_0: _, // twenty-four   = 1
                                                                               // point         = 0
       x_sf2_3: _, x_sf2_2: _, x_sf2_1: T, x_sf2_0: T,                         // three         = 2
-      x_sf3_3: _, x_sf3_2: _, x_sf3_1: T, x_sf3_0: _,                         // two           = 0
 
       y_sf1_5: _, y_sf1_4: T, y_sf1_3: T, y_sf1_2: _, y_sf1_1: _, y_sf1_0: T, // twenty-five   = 2
                                                                               // point         = 0
       y_sf2_3: _, y_sf2_2: _, y_sf2_1: _, y_sf2_0: T,                         // one           = 1
-      y_sf3_3: _, y_sf3_2: _, y_sf3_1: _, y_sf3_0: _,                         // ____          = 0
 
       z_sf1_5: _, z_sf1_4: T, z_sf1_3: T, z_sf1_2: _, z_sf1_1: T, z_sf1_0: _, // twenty-six    = 1
                                                                               // point         = 0
       z_sf2_3: T, z_sf2_2: _, z_sf2_1: _, z_sf2_0: T,                         // nine          = 1
-      z_sf3_3: T, z_sf3_2: _, z_sf3_1: _, z_sf3_0: _                          // eight         = 1
 
-                                                                              // Total: 83
+                                                                              // Total: 78
     });
 
     expect(result).toEqual({
       sum11: _, sum10: _, sum9: _, sum8: _, sum7: _, sum6: T,
-      sum5: _, sum4: T, sum3: _, sum2: _, sum1: T, sum0: T
+      sum5: _, sum4: _, sum3: T, sum2: T, sum1: _, sum0: _
     });
   });
 });
